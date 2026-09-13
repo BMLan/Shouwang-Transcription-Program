aad7e28cb2986f2e5aac69f98b945088b0b89b4e
ytzhang0587 <1160559757@qq.com>
2026-09-13 15:37:34 +0800

    Update md_batch_gen.bat

--- md_batch_gen.bat
+++ md_batch_gen.bat
@@ -11,15 +11,15 @@ md %folder_name%
 echo Folder %folder_name% Created.
 
 :: 声明空白变量记录文件名，进入创建的文件夹
-set file_name=
+set file_name="foo.md"
 cd %folder_name%
 
 :: 批量创建空白md文档
+:: 章节号为1位时，补齐1位前导零
+:: 章节号为2位时，正常创建文件
 for /l %%i in (1,1,%ch%) do (
-    :: 章节号为1位时，补齐1位前导零
     if %%i LSS 10 (
         set file_name="VOL%vol%_CH0%%i.md"
-    :: 章节号为2位时，正常创建文件
     ) else if %%i GEQ 10 (
         set file_name="VOL%vol%_CH%%i.md"
     )
